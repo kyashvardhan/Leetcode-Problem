08107e45af5546a09dfd9575a255f3649bbc5047
Yashvardhan Kapse <68836374+kyashvardhan@users.noreply.github.com>
2025-05-29 23:45:59 +0530

    Create generate_permutations.py
    add backtracking solution to generate all permutations of distinct integers (Leetcode 46)

---
+++ set-1/generate_permutations.py
@@ -0,0 +1,19 @@
+def permute(nums):
+    result = []
+    
+    def backtrack(path, used):
+        if len(path) == len(nums):
+            result.append(path[:])
+            return
+        
+        for i in range(len(nums)):
+            if not used[i]:
+                used[i] = True
+                path.append(nums[i])
+                backtrack(path, used)
+                path.pop()
+                used[i] = False
+    
+    used = [False] * len(nums)
+    backtrack([], used)
+    return result
