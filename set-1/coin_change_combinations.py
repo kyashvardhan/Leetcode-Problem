2ad2bb7242958d973ce0f31fc222e34f69c33ed3
Yashvardhan Kapse <68836374+kyashvardhan@users.noreply.github.com>
2025-06-17 23:55:10 +0530

    Create coin_change_combinations.py

---
+++ set-1/coin_change_combinations.py
@@ -0,0 +1,9 @@
+def change(amount, coins):
+    dp = [0] * (amount + 1)
+    dp[0] = 1  # base case: one way to make amount 0
+
+    for coin in coins:
+        for x in range(coin, amount + 1):
+            dp[x] += dp[x - coin]
+
+    return dp[amount]
